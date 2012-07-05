ded6d5ded727f15859a53920034bb473e12dfff7
Dasha <darya240892@gmail.com>
2012-07-05 11:08:25 +0400

    Piston created

---
+++ Assets/_Prefabs/Scene Objects/Piston/MovePiston.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovePiston : MonoBehaviour
+{
+	public float rate;
+	public float Maxlength;
+	public float Minlength;
+	private bool isMovingUp = true;
+	Transform pisty;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		pisty = transform.Find ("Pisty");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (isMovingUp) {
+			if (pisty.localPosition.y >= Maxlength) {
+				isMovingUp = false;
+			} else {
+				Vector3 movement = Vector3.up * rate * Time.deltaTime;
+				movement = transform.localRotation * movement;
+				pisty.rigidbody.MovePosition (pisty.rigidbody.position + movement);
+			}
+		} else {
+			if (pisty.localPosition.y <= Minlength) {
+				isMovingUp = true;
+			} else {
+				Vector3 movement = Vector3.down * rate * Time.deltaTime;
+				movement = transform.localRotation * movement;
+				pisty.rigidbody.MovePosition (pisty.rigidbody.position + movement);
+			}
+		}
+	}
+}
