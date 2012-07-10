fcdb7319de8e05c109813557dc5022809c79b3e8
Bangalor <4l3x4nd3r.dy0m1n@gmail.com>
2012-07-10 12:39:24 +0400

    Raycast fixed

--- Assets/Scripts/EnemiesPath.cs
+++ Assets/Scripts/EnemiesPath.cs
@@ -14,28 +14,44 @@ public class EnemiesPath : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-			currentTarget=GetNearlyPoint();
+		currentTarget=GetNearlyPoint();
 		lasttarget=targets[currentTarget];
+		
 	}
 	// Update is called once per frame
 	void Update ()
-	{
-		Ray ray = new Ray(transform.position,new Vector3(0,0,1));
-		RaycastHit hit;
-		if (Physics.Raycast(ray,out hit, distance)){
-			Debug.Log(hit.collider.tag);
-			if (hit.collider.tag=="Player"){
-				PlayerIsVisible=true;
-			}else{
-				PlayerIsVisible=false;
+	{	
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		Vector3 playerDirection = (player.transform.position-transform.position).normalized;
+		//Debug.DrawLine(transform.position,player.transform.position);
+		float playerAngle = Vector3.Angle(playerDirection,transform.forward.normalized);
+		//Debug.Log(playerAngle);
+		if (playerAngle<45){
+			Ray ray = new Ray (transform.position,playerDirection);
+			//Debug.DrawRay(transform.position, playerDirection,Color.white,15f);
+			RaycastHit hit;
+			if (Physics.Raycast(ray,out hit)){
+				//Debug.DrawLine(transform.position,hit.point);
+				if (hit.collider.tag=="Player"){
+					PlayerIsVisible=true;
+				//	Debug.Log("I see you");
+				} else {
+					PlayerIsVisible=false;
+				//	Debug.Log("I can not see your. reason: "+hit.collider.name);
+				}
+			
 			}
+		} else {
+			//Debug.Log("Can not see");
+			//Do attack
 		}
 		if (PlayerIsAttackable || PlayerIsVisible) {
-			GameObject player = GameObject.FindGameObjectWithTag ("Player");
 			lasttarget = player;
+			
 			Vector3 pointcoor = player.transform.position;
 			Vector3 thiscoor = this.transform.position;
-		
+			transform.LookAt(pointcoor);
+			Debug.DrawRay(transform.position,transform.forward);
 			Vector3 nextStep = new Vector3 (pointcoor.x - thiscoor.x, 0, pointcoor.z - thiscoor.z).normalized * 5;
 			if (nextStep.magnitude < new Vector3 (pointcoor.x - thiscoor.x, 0, pointcoor.z - thiscoor.z).magnitude) {
 				transform.position += nextStep * Time.deltaTime;
@@ -51,7 +67,7 @@ public class EnemiesPath : MonoBehaviour
 			Vector3 pointcoor = targets [currentTarget].transform.position;
 			lasttarget = targets [currentTarget];
 			
-			//this.transform.LookAt (targets [currentTarget].transform);
+			this.transform.LookAt (pointcoor);
 			Vector3 thiscoor = this.transform.position;
 			Vector3 nextStep = new Vector3 (pointcoor.x - thiscoor.x, 0, pointcoor.z - thiscoor.z).normalized * 5;
 			if (nextStep.magnitude > new Vector3 (pointcoor.x - thiscoor.x, 0, pointcoor.z - thiscoor.z).magnitude) {
